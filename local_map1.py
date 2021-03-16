c1079f1e766347bffe4ae5b32211c898c84fa7d5
Jayabrata(Joy) <78541698+jayabrata97@users.noreply.github.com>
2021-03-16 15:51:20 +0530

    local map without dynamic agent

--- local_map1.py
+++ local_map1.py
@@ -1,40 +1,46 @@
-#############################################################################################
-# 1. It is working but I am checking for all grids which are in sensing range to find sen_grid.
-# This is not good because for big map it can take time to find this list. How to make it more efficient?
-# 2. Till now I have only implemented for static map without other agents. How to generalize it to 
+##################################################################################################
+# 1. Till now I have only implemented for static map without other agents. How to generalize it to 
 # dynamic local map? 
+# 2. Also one small thing: Sensing area should be circular with radius equal to sensing range. 
+# Currently I am using square for sensing area not circular. 
 ################################################################################################
 from map import grid
 
-c_pose = [1,2] # later use c_pose instead of init
-sense = 7  # Set the sensing range here
+c_pose = [3,4] # later use c_pose instead of init
+sense = 7    # Define the sensing range here 
 obs = [[1 for col in range(15)] for row in range(15)]
 #print(obs)
-########################################################################################
-#Trial: 4
-#shift_x = 2 - c_pose[0]
-#shift_y = 2 - c_pose[1]
-#for row in range(len(obs)):
- #   for col in range(len(obs[0])):
-  #      if (row+shift_x)>=0 and (col+shift_y)>=0 and (col+shift_y)<len(obs[0]) and (row+shift_x)<len(obs):
-   #         obs[row+shift_x][col+shift_y] = grid[row][col]
-    #        print(obs)
-     #   else:
-      #      obs[(row+shift_x)%(len(obs))][(col+shift_y)%(len(obs[0]))]=1    
-       #     print(obs)
-#obs[sense][sense] = 1
-#print(obs)
-###########################################################################################
+##################################################################################
 # Trial: 5
+#shift_x = sense - c_pose[0]
+#shift_y = sense - c_pose[1]
+#sen_grid = []
+#for row in range(len(grid)):
+ #   for col in range(len(grid[0])):
+  #      if abs(c_pose[0]-row) <= sense and abs(c_pose[1]-col) <= sense:
+   #         sen_grid.append([row,col])
+#print(sen_grid)
+#print(sen_grid[0][1]) 
+#for i in range(len(sen_grid)):
+ #   obs[sen_grid[i][0]+shift_x][sen_grid[i][1]+shift_y] = grid[sen_grid[i][0]][sen_grid[i][1]]
+#obs[sense][sense] = 1
+#print(obs)    
+#######################################################################################################
+#Trial: 6
 shift_x = sense - c_pose[0]
 shift_y = sense - c_pose[1]
 sen_grid = []
-for row in range(len(grid)):
-    for col in range(len(grid[0])):
-        if abs(c_pose[0]-row) <= sense and abs(c_pose[1]-col) <= sense:
-            sen_grid.append([row,col])
-#print(sen_grid)
-#print(sen_grid[0][1]) 
+a = []
+for i in range(2*sense+1):
+    a.append((-sense+i))
+#print(a)
+for i in range(2*sense+1):
+    for j in range(2*sense+1):
+        x = c_pose[0]
+        y = c_pose[1]
+        if x+a[i]>=0 and y+a[j]>=0 and x+a[i]<=len(grid)-1 and y+a[j]<=len(grid[0])-1:
+            sen_grid.append([(x+a[i]), (y+a[j])])                   
+#print(sen_grid) 
 for i in range(len(sen_grid)):
     obs[sen_grid[i][0]+shift_x][sen_grid[i][1]+shift_y] = grid[sen_grid[i][0]][sen_grid[i][1]]
 obs[sense][sense] = 1
